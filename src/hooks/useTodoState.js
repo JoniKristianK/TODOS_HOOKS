b5e4498b4faa1a4f0467a27f270d06d95c173ccb
YosKa <joni.karilainen@gmail.com>
2020-07-31 14:33:17 +0300

    adding all files

---
+++ src/hooks/useTodoState.js
@@ -0,0 +1,26 @@
+import useLocalStorageState from './useLocalStorageState';
+import { v4 as uuidv4 } from 'uuid';
+
+export default (initialTodos) => {
+  const [todos, setTodos] = useLocalStorageState('todos', initialTodos);
+  return {
+    todos,
+    addTodo: (newTodoText) => {
+      setTodos([...todos, { id: uuidv4(), task: newTodoText, completed: false }]);
+    },
+    removeTodo: (todoId) => {
+      //filter out removed todo
+      const updatedTodos = todos.filter((todo) => todo.id !== todoId);
+      //call setTodos with new todos array
+      setTodos(updatedTodos);
+    },
+    toggleTodo: (todoId) => {
+      const updateTodos = todos.map((todo) => (todo.id === todoId ? { ...todo, completed: !todo.completed } : todo));
+      setTodos(updateTodos);
+    },
+    editTodo: (todoId, newTask) => {
+      const updateTodos = todos.map((todo) => (todo.id === todoId ? { ...todo, task: newTask } : todo));
+      setTodos(updateTodos);
+    },
+  };
+};
